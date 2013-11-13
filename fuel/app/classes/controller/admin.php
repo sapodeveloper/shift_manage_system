e4f3b7ed0db414c6cea394844dd523f2f0b97c60
morishita <tomohiro.m0219@gmail.com>
2013-11-13 14:46:52 +0900

    #17 アクセス権限の変更とリンク作成

--- fuel/app/classes/controller/admin.php
+++ fuel/app/classes/controller/admin.php
@@ -4,9 +4,9 @@ class Controller_Admin extends Controller
 {
 	public function before()
 	{
-		if (!Auth::check() && Auth::member(6))
+		if (!Auth::check() && !Auth::member(6))
 		{
-		    Response::redirect('/main');
+		  Response::redirect('/main');
 		}
 	}
 }
