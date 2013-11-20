e24b34ce762fb269705a2b5568bed00eee14a5d8
morishita <tomohiro.m0219@gmail.com>
2013-11-20 15:25:21 +0900

    #17 独自権限定義によるアクセス制限の変更

--- fuel/app/classes/controller/admin.php
+++ fuel/app/classes/controller/admin.php
@@ -4,9 +4,13 @@ class Controller_Admin extends Controller
 {
 	public function before()
 	{
-		if (!Auth::check() && !Auth::member(6))
+		if (Auth::check())
 		{
-		  Response::redirect('/main');
+			$user_id = Auth::get('id');
+			$user_auth_type = Model_User::find($user_id);
+			if($user_auth_type->auth_id != 3){
+				Response::redirect('/main');
+			}
 		}
 	}
 }
