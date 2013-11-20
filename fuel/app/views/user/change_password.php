7f67c8663ca0b6bcb00c577d1534fdd704fbfaaf
morishita <tomohiro.m0219@gmail.com>
2013-11-20 16:13:12 +0900

    #17 ログインユーザの情報変更とパスワード変更機能実装

---
+++ fuel/app/views/user/change_password.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+<h2><?php echo $user->full_name; ?>さんのパスワード変更</h2>
+<div class="uk-grid">
+	<div class="uk-width-1-2">
+		<?php echo Form::open(array("action" => "user/change_password")); ?>
+			<table class="uk-table">
+				<tr>
+					<td class="uk-width-1-3">現パスワード</td>
+					<td>
+						<?php echo Form::password('old_password', Input::post('old_password', ''), array('class' => '')); ?>
+					</td>
+				</tr>
+				<tr>
+					<td class="uk-width-1-3">新パスワード</td>
+					<td>
+						<?php echo Form::password('new_password', Input::post('new_password', ''), array('class' => '')); ?>
+					</td>
+				</tr>
+				<tr>
+					<td class="uk-width-1-3">新パスワード(再度)</td>
+					<td>
+						<?php echo Form::password('check_new_password', Input::post('check_new_password', ''), array('class' => '')); ?>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" align="center"><?php echo Form::submit('submit', 'Update', array('class' => '')); ?></td>
+				</tr>
+			</table>
+		<?php echo Form::close(); ?>	
+	</div>
+</div>
+<?php echo Html::anchor('user', '戻る'); ?>
--- fuel/app/views/user/index.php
+++ fuel/app/views/user/index.php
@@ -1,2 +1,36 @@
 <meta charset="utf-8">
-<h2>ユーザ情報</h2>
+<h2><?php echo $user->full_name; ?>さんのユーザ情報</h2>
+<div class="uk-grid">
+	<div class="uk-width-1-2">
+		<table class="uk-table">
+			<tr>
+				<td>ログイン名</td>
+				<td><?php echo $user->username; ?></td>
+			</tr>
+			<tr>
+				<td>スタッフ名</td>
+				<td><?php echo $user->full_name; ?></td>
+			</tr>
+			<tr>
+				<td>メールアドレス</td>
+				<td><?php echo $user->email; ?></td>
+			</tr>
+			<tr>
+				<td>学部</td>
+				<td><?php echo $user->department->department_name; ?></td>
+			</tr>
+			<tr>
+				<td>学科</td>
+				<td><?php echo $user->cource->cource_name; ?></td>
+			</tr>
+			<tr>
+				<td>入学年度</td>
+				<td><?php echo $user->year; ?></td>
+			</tr>
+		</table>
+	</div>
+</div>
+<br>
+<?php echo Html::anchor('user/edit', 'ユーザ情報編集', array('class' => 'uk-button')); ?> 
+<?php echo Html::anchor('user/change_password', 'パスワード変更', array('class' => 'uk-button')); ?><br>
+<i class="fa fa-users"></i> <?php echo Html::anchor('/main', '戻る'); ?><br>
