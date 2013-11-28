455bd79fe660da62ea7a51ed9aaad00683dbaf15
morishita <tomohiro.m0219@gmail.com>
2013-11-28 15:45:24 +0900

    シフト申請と確定シフトのダミーデータ生成

--- fuel/app/migrations/015_create_irregular_user.php
+++ fuel/app/migrations/015_create_irregular_user.php
@@ -17,6 +17,22 @@ class Create_irregular_user
 			'user_comment' => array('type' => 'text'),
 
 		), array('id'));
+
+		$table = 'irregular_user';
+		$end_time = array(6, 7, 8, 9);
+		for ($irregular_shift = 1; $irregular_shift <= 2; $irregular_shift++){
+			for($loop = 1; $loop <= 10; $loop++){
+				for ($user_id=  1; $user_id <= 6 ; $user_id++) { 
+					\DB::insert($table)->set(array('user_id' => $user_id, 
+																		 'irregular_day_id' => $loop,
+																		 'request_start' => 1,
+																		 'request_end' => $end_time[array_rand($end_time)],
+																		 'edited_start' => 1,
+																		 'edited_end' => $end_time[array_rand($end_time)],
+																		 'user_comment' => "test data"))->execute();
+				}
+			}
+		}
 	}
 
 	public function down()
