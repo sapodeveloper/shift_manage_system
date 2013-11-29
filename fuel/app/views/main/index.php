f602b2cc7bc31e4e0b07249b8fb3a55ded523c7b
morishita <tomohiro.m0219@gmail.com>
2013-11-29 22:58:45 +0900

    #28 とりあえず、枠だけ作ってみる

--- fuel/app/views/main/index.php
+++ fuel/app/views/main/index.php
@@ -1,2 +1,20 @@
 ログイン後のページ
 <br>
+
+<h3>確定シフト</h3>
+<table class="uk-table uk-table-hover">
+	<thead>
+		<tr>
+			<th>シフト名</th>
+			<th></th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>テストイレギュラーシフト</td>
+			<td><a href="#">確認</a></td>
+			<td><a href="#">PDF</a></td>
+		</tr>
+	</tbody>
+</table>
