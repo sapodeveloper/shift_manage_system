cbdd604c25c7f5d50ce9a3f4a9f39c457d51a50b
morishita <tomohiro.m0219@gmail.com>
2013-11-29 23:34:52 +0900

    #28 申請可能枠と確定枠を実装

--- fuel/app/views/main/index.php
+++ fuel/app/views/main/index.php
@@ -1,7 +1,33 @@
 ログイン後のページ
 <br>
+<?php if($receiving_irregulars): ?>
+<h3>申請受付中イレギュラーシフト</h3>
+<table class="uk-table uk-table-hover">
+	<thead>
+		<tr>
+			<th>シフト名</th>
+			<th>申請期限</th>
+			<th></th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($receiving_irregulars as $r_irregular): ?>
+			<tr>
+				<td><?php echo $r_irregular->irregular_name; ?></td>
+				<td><?php echo date( 'Y年m月d日 H時i分', strtotime($r_irregular->irregular_limitdate)); ?></td>
+				<td><a href="#">申請</a></td>
+				<td><a href="#">確認</a></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+<?php endif; ?>
 
-<h3>確定シフト</h3>
+<br>
+
+<?php if($decision_irregulars): ?>
+<h3>確定イレギュラーシフト</h3>
 <table class="uk-table uk-table-hover">
 	<thead>
 		<tr>
@@ -11,10 +37,13 @@
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
-			<td>テストイレギュラーシフト</td>
-			<td><a href="#">確認</a></td>
-			<td><a href="#">PDF</a></td>
-		</tr>
+		<?php foreach ($decision_irregulars as $irregular): ?>
+			<tr>
+				<td><?php echo $irregular->irregular_name; ?></td>
+				<td><a href="#">確認</a></td>
+				<td><a href="#">PDF</a></td>
+			</tr>
+		<?php endforeach; ?>
 	</tbody>
 </table>
+<?php endif; ?>
