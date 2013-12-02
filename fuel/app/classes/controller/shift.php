40f49fa273463e6209eb287744c12ec87fc8884f
morishita <tomohiro.m0219@gmail.com>
2013-12-02 18:31:42 +0900

    #34 シフト申請アクションを作成

--- fuel/app/classes/controller/shift.php
+++ fuel/app/classes/controller/shift.php
@@ -13,4 +13,14 @@ class Controller_Shift extends Controller_Application
 		return $view;
 	}
 
+	public function action_request()
+	{
+		$view = View::forge('layout/application');
+		$view->header = View::forge('layout/header');
+		$view->left_side_menu = View::forge('layout/left_side_menu');
+		$view->contents = View::forge('shift/request');
+		$view->footer = View::forge('layout/footer');
+		return $view;
+	}
+
 }
