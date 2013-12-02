61c3a79ea211c038e9e5fd88bf24f8f3ff918e16
morishita <tomohiro.m0219@gmail.com>
2013-12-02 18:16:14 +0900

    #32 イレギュラーシフトの新規作成アクション部分だけ作成

--- fuel/app/views/manage/shift/new.php
+++ fuel/app/views/manage/shift/new.php
@@ -1,1 +1,5 @@
 <h3>シフト新規作成</h3>
+
+<?php echo Html::anchor('#', '新規レギュラーシフト', array('class' => 'uk-button uk-button-large uk-button-primary')); ?>
+<br><br>
+<?php echo Html::anchor('manage/shift/irregular/create', '新規イレギュラーシフト', array('class' => 'uk-button uk-button-large uk-button-danger')); ?>
