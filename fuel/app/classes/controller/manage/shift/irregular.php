7b401d6400cbf2069d0d0d0bc2bb50b6d3be0288
Y.Mizawa <b113229@cc.it-hiroshima.ac.jp>
2013-12-04 18:23:55 +0900

    途中

--- fuel/app/classes/controller/manage/shift/irregular.php
+++ fuel/app/classes/controller/manage/shift/irregular.php
@@ -5,6 +5,34 @@ class Controller_Manage_Shift_Irregular extends Controller_Manage_Shift
 
 	public function action_create()
 	{
+		if(Input::method() == 'POST'){
+			・オブジェクトの生成
+			Auth::instance()->create_user(Input::post('username'), 'saposen', Input::post('email'));
+
+			・データを取得してオブジェクトのハッシュに代入
+
+			$->set(array(
+				'' => Input::post(''),
+				'' => Input::post(''),
+				'' => Input::post(''),
+				'' => Input::post(''),
+				'' => Input::post(''),
+				'' => Input::post(''),
+				'' => Input::post(''),
+			));
+
+		if ($user->save())
+			{
+				Response::redirect('');
+			}
+
+			else
+			{
+				Response::redirect('');
+			}
+			・ヒントはadmin/userやuserのコントローラを参考にしてねby　edyとよぽー
+		}
+
 		$view = View::forge('layout/application');
 		$view->header = View::forge('layout/header');
 		$view->left_side_menu = View::forge('layout/left_side_menu');
