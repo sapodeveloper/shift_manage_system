7d18be6ffbe68d1de2bb514ce4787c33f06c9c20
Y.Mizawa <b113229@cc.it-hiroshima.ac.jp>
2013-12-06 15:05:23 +0900

    その２

--- fuel/app/classes/controller/manage/shift/irregular.php
+++ fuel/app/classes/controller/manage/shift/irregular.php
@@ -6,19 +6,21 @@ class Controller_Manage_Shift_Irregular extends Controller_Manage_Shift
 	public function action_create()
 	{
 		if(Input::method() == 'POST'){
+
 			・オブジェクトの生成
-			Auth::instance()->create_user(Input::post('username'), 'saposen', Input::post('email'));
+
+			Auth::instance()->create_irregular;
 
 			・データを取得してオブジェクトのハッシュに代入
 
 			$->set(array(
-				'' => Input::post(''),
-				'' => Input::post(''),
-				'' => Input::post(''),
-				'' => Input::post(''),
-				'' => Input::post(''),
-				'' => Input::post(''),
-				'' => Input::post(''),
+				'id' => Input::post('id'),
+				'irregular_name' => Input::post('irregular_name'),
+				'irregular_enabledate' => Input::post('irregular_enabledate'),
+				'irregular_limitdate' => Input::post('irregular_limitdate'),
+				'create_at' => Input::post('create_at'),
+				'update_at' => Input::post('update_at'),
+				'irregular_condition' => 1,
 			));
 
 		if ($user->save())
