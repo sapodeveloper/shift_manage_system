f0ffbc6e7bf4f767843572beeb590d7baeae2c9c
morishita <tomohiro.m0219@gmail.com>
2013-12-18 11:16:39 +0900

    #46 タブ表示の部分の修正

--- fuel/app/views/manage/shift/irregular/edit.php
+++ fuel/app/views/manage/shift/irregular/edit.php
@@ -8,17 +8,16 @@
 		$.ajax(url, {"complete": function(xhr,status){
 			$("#irregular_shift_day").hide();
 			window.xhr = xhr;
-			$("#my_id").after($(xhr.responseText));
+			$("#tab").after($(xhr.responseText));
 		}});
 	}
 </script>
 [<?php echo $irregular_shift->irregular_name; ?>]の編集
 <br><br>
-<ul class="uk-tab uk-tab-grid uk-tab-bottom" data-uk-tab="{connect:'#my-id'}">
+<ul class="uk-tab uk-tab-grid uk-tab-bottom" id="tab">
 	<?php foreach ($irregular_shift_days as $irregular_shift_day): ?>
 		<li class="uk-width-1-<?php echo count(${'irregular_shift_days'}); ?>">
 			<a class="select_day" id="<?php echo $irregular_shift_day->id; ?>"><?php echo $irregular_shift_day->irregular_day_name; ?></a>
 		</li>
 	<?php endforeach; ?>
 </ul>
-<div id="my_id"></div>
