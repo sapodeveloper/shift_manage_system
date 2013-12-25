6cb13c9a3cf40804915ad7c8dab2d7f0589bac25
morishita <tomohiro.m0219@gmail.com>
2013-12-25 12:58:48 +0900

    #68 footer忘れてた

--- fuel/app/views/auth/login.php
+++ fuel/app/views/auth/login.php
@@ -69,5 +69,12 @@
 			</div>
 		</div>
 	</div>
+	<br>
+	<!-- footer -->
+	<footer>
+		<div style="text-align: center; ">
+			<h6>Copyright ©<?php echo date('Y'); ?> HIT ISMC Support Center All Rights Reserved.</h6>
+		</div>
+	</footer>
 </body>
 </html>
