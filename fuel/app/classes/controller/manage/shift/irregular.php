85c7b79973af9c7e5d3d9db717b5205f3b840608
Y.Mizawa <b113229@cc.it-hiroshima.ac.jp>
2014-01-15 15:59:57 +0900

    2014-1-15 1

--- fuel/app/classes/controller/manage/shift/irregular.php
+++ fuel/app/classes/controller/manage/shift/irregular.php
@@ -5,6 +5,48 @@ class Controller_Manage_Shift_Irregular extends Controller_Manage_Shift
 
 	public function action_create()
 	{
+		if(Input::method() == 'POST')
+		{
+			$limitdate = Input::post('limitdate');
+			$today = date("Y-m-d");
+			$irregular = Model_Irregular::forge(array(
+				'irregular_name' => Input::post('irregular_name'),
+				'irregular_limitdate' => date("Y-m-d 23:59:59",strtotime("$limitdate")),
+				'irregular_enabledate' => $today,
+				'irregular_condition' => 1,
+				'updated_at' => 1,
+			));
+
+			if ($irregular->save())
+				{}
+				else
+				{
+					Session::set_flash('error', '失敗');
+				}
+
+			$irregular_id = Model_Irregular::find('last');
+			$irregular_day_date = Input::post('irregular_date');
+
+			foreach($irregular_day_date as $date){
+					$irregular_date = Model_Irregular_Day::forge(array(
+						'irregular_id' => $irregular_id->id,
+						'irregular_day_date' => $date,
+						'irregular_day_name' => date("n月j日",strtotime("$date")),
+						'irregular_day_condition' => 1,
+						'updated_at' => 0,
+					));
+
+			if ($irregular_date->save())
+				{}
+				else
+				{
+					Session::set_flash('error', '失敗');
+				}
+			}
+
+			Response::redirect('shift/check'); 
+		}
+
 		$view = View::forge('layout/application');
 		$view->contents = View::forge('manage/shift/irregular/create');
 		return $view;
