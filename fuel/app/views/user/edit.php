5f8bb17f42774adb46a23cb1beba27e0f3e1a10c
morishita <tomohiro.m0219@gmail.com>
2014-01-21 11:27:13 +0900

    #92 ユーザ情報編集画面に新デザイン適応

--- fuel/app/views/user/edit.php
+++ fuel/app/views/user/edit.php
@@ -1,53 +1,63 @@
 <?php
 	$condition_data = array(0 => '無効', 1 => '有効');
 ?>
-<h2><?php echo $user->full_name; ?>さんのスタッフ情報編集</h2>
-<div class="uk-grid">
-	<div class="uk-width-1-2">
-		<?php echo Form::open(array("action" => "user/edit")); ?>
-			<table class="uk-table">
-				<tr>
-					<td class="uk-width-1-3">ログイン名(学生番号)</td>
-					<td>
-						<?php echo Form::input('username', Input::post('username', isset($user) ? $user->username : ''), array('class' => '', 'placeholder'=>'学生番号')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td>スタッフ名</td>
-					<td>
-						<?php echo Form::input('frist_name', Input::post('frist_name', isset($user) ? $user->frist_name : ''), array('class' => '', 'placeholder'=>'名字')); ?>
-						<?php echo Form::input('last_name', Input::post('last_name', isset($user) ? $user->last_name : ''), array('class' => '', 'placeholder'=>'名前')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td>メールアドレス</td>
-					<td>
-						<?php echo $user->email; ?>
-					</td>
-				</tr>
-				<tr id="department">
-					<td>学部</td>
-					<td>
-						<?php echo Form::select('department_id', $user->department_id, $department_data, array('class' => '', 'id' => 'department_id')); ?>
-					</td>
-				</tr>
-				<tr id="cource_list">
-					<td>学科</td>
-					<td>
-						<?php echo Form::select('cource_id', $user->cource_id, $cource_data, array('class' => '', 'id' => 'cource_id')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td>入学年度</td>
-					<td>
-						<?php echo Form::input('year', Input::post('year', isset($user) ? $user->year : ''), array('class' => '', 'placeholder'=>'入学年度')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2" align="center"><?php echo Form::submit('submit', 'Update', array('class' => '')); ?></td>
-				</tr>
-			</table>
-		<?php echo Form::close(); ?>	
+<center><h1 class="uk-article-title"><?php echo $user->full_name; ?>さんのスタッフ情報編集</h1></center>
+<br>
+<?php echo Form::open(array("action" => "user/edit")); ?>
+<fieldset>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;ログイン名（学生番号）</div>
+		<div class="uk-width-medium-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo Form::input('username', Input::post('username', isset($user) ? $user->username : ''), array('class' => '', 'placeholder'=>'学生番号')); ?></div>
 	</div>
-</div>
+	<HR noshade>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;スタッフ名</div>
+		
+		<div class="uk-width-medium-3-6">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo Form::input('frist_name', Input::post('frist_name', isset($user) ? $user->frist_name : ''), array('class' => '', 'placeholder'=>'名字', 'size' => 5)); ?>
+						&nbsp;&nbsp;<?php echo Form::input('last_name', Input::post('last_name', isset($user) ? $user->last_name : ''), array('class' => '', 'placeholder'=>'名前', 'size' => 5)); ?>
+		</div>
+	</div>
+	<HR noshade>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-envelope-o"></i>&nbsp;メールアドレス</div>
+		<div class="uk-width-medium-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $user->email; ?></div>
+	</div>
+	<HR noshade>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;学部</div>
+		<div class="uk-width-medium-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<?php echo Form::select('department_id', $user->department_id, $department_data, array('class' => '', 'id' => 'department_id')); ?>
+		</div>
+	</div>
+	<HR noshade>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;学科</div>
+		<div class="uk-width-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<?php echo Form::select('cource_id', $user->cource_id, $cource_data, array('class' => '', 'id' => 'cource_id')); ?>
+		</div>
+	</div>
+	<HR noshade>
+	<div class="uk-grid" data-uk-grid-match>
+		<div class="uk-width-medium-1-6">&nbsp;</div>
+		<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;入学年度</div>
+		<div class="uk-width-medium-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo Form::input('year', Input::post('year', isset($user) ? $user->year : ''), array('class' => '', 'placeholder'=>'入学年度')); ?></div>
+	</div>
+	<HR noshade>
+	<div class="uk-grid">
+		<div class="uk-width-1-3">&nbsp;</div>
+		<div class="uk-width-1-3">
+			<button class="uk-button uk-button-primary uk-button-expand uk-button-large" type"submit">
+				Update&nbsp;<i class="fa fa-sign-in"></i>
+			</button>												
+	</div>
+
+</fieldset>
+<?php echo Form::close(); ?>	
+
 <?php echo Html::anchor('user', '戻る'); ?>
