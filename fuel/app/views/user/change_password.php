081529fa1da425870d5ee12730135397222a40a7
morishita <tomohiro.m0219@gmail.com>
2014-01-21 15:29:05 +0900

    #94 パスワード変更画面に新デザイン適用

--- fuel/app/views/user/change_password.php
+++ fuel/app/views/user/change_password.php
@@ -1,31 +1,23 @@
-<h2><?php echo $user->full_name; ?>さんのパスワード変更</h2>
-<div class="uk-grid">
-	<div class="uk-width-1-2">
-		<?php echo Form::open(array("action" => "user/change_password")); ?>
-			<table class="uk-table">
-				<tr>
-					<td class="uk-width-1-3">現パスワード</td>
-					<td>
-						<?php echo Form::password('old_password', Input::post('old_password', ''), array('class' => '')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="uk-width-1-3">新パスワード</td>
-					<td>
-						<?php echo Form::password('new_password', Input::post('new_password', ''), array('class' => '')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="uk-width-1-3">新パスワード(再度)</td>
-					<td>
-						<?php echo Form::password('check_new_password', Input::post('check_new_password', ''), array('class' => '')); ?>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2" align="center"><?php echo Form::submit('submit', 'Update', array('class' => '')); ?></td>
-				</tr>
-			</table>
-		<?php echo Form::close(); ?>	
-	</div>
-</div>
+<center>
+	<h1 class="uk-article-title"> パスワード変更画面 </h1>
+		<br>
+	<?php echo Form::open(array("action" => "user/change_password")); ?>		<fieldset>
+			<i class="fa fa-key"></i> 旧パスワード　<?php echo Form::password('old_password', Input::post('old_password', ''), array('class' => '')); ?>
+				<br>
+				<br>
+			<i class="fa fa-key"></i> 新パスワード　<?php echo Form::password('new_password', Input::post('new_password', ''), array('class' => '')); ?>
+				<br>
+				<br>
+			<i class="fa fa-key"></i> 再入力　　　　<?php echo Form::password('check_new_password', Input::post('check_new_password', ''), array('class' => '')); ?>
+				<br>
+				<br>
+			<div style="text-align:">
+				<button style="text-align: center;" class="uk-button-primary uk-h3" type="submit">パスワードを変更する
+					<i class="fa fa-external-link"></i>
+				</button>
+			</div>
+		</fieldset>
+	<?php echo Form::close(); ?>
+</center>
+
 <?php echo Html::anchor('user', '戻る'); ?>
