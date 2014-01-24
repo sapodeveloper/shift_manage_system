a55c3b1ad06c91376b66bcced883ecb201fceed1
morishita <tomohiro.m0219@gmail.com>
2014-01-24 20:17:28 +0900

    #108 イレギュラーシフトの表示修正

--- fuel/app/views/manage/shift/editable_list.php
+++ fuel/app/views/manage/shift/editable_list.php
@@ -1,6 +1,5 @@
 <h3>編集可能シフト一覧</h3>
 <?php if($receiving_irregulars): ?>
-<h3>申請受付中イレギュラーシフト</h3>
 <table class="uk-table uk-table-hover">
 	<thead>
 		<tr>
@@ -12,10 +11,10 @@
 	</thead>
 	<tbody>
 		<?php foreach ($receiving_irregulars as $r_irregular): ?>
-			<tr>
+			<tr class="<?php Helper_Shift::shift_condition_color($r_irregular->irregular_condition); ?>">
 				<td><?php echo $r_irregular->irregular_name; ?></td>
 				<td><?php echo date( 'Y年m月d日', strtotime($r_irregular->irregular_limitdate)); ?></td>
-				<td></td>
+				<td><?php Helper_Shift::shift_condition($r_irregular->irregular_condition); ?></td>
 				<td><?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$r_irregular->id.'/info', 'シフト編集', array('class' => 'uk-button')); ?></td>
 				<td><a href="#" class="uk-button">申請受付を終了する</a></td>
 			</tr>
