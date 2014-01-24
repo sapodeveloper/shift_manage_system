1cc277d4e1e072b4043d956f836ba24c03eb8be9
nishimoto <b111090@cc.it-hiroshima.ac.jp>
2014-01-24 20:13:52 +0900

    ユーザの新規登録画面にモックアップを適応

--- fuel/app/views/admin/user/cource_list.php
+++ fuel/app/views/admin/user/cource_list.php
@@ -1,6 +1,8 @@
-<tr id="cource_list">
-	<td>学科</td>
-	<td>
+<HR noshade>
+<div id="cource_list" class="uk-grid" data-uk-grid-match>
+	<div class="uk-width-medium-1-6">&nbsp;</div>
+	<div class="uk-width-medium-2-6"><i class="fa fa-tag"></i>&nbsp;学科</div>
+	<div class="uk-width-3-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<?php echo Form::select('cource_id', null, $cource_data, array('class' => '', 'id' => 'cource_id')); ?>
-	</td>
-</tr>
+	</div>
+</div>
