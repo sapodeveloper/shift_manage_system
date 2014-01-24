9b940c9002838e8633bd0aecde33e1a484be917a
morishita <tomohiro.m0219@gmail.com>
2014-01-24 18:49:47 +0900

    #104 イレギュラーシフトの出力をajax化

--- fuel/app/views/shift/irregular/detail.php
+++ fuel/app/views/shift/irregular/detail.php
@@ -1,9 +1,8 @@
 <h3><?php echo $irregular_shift->irregular_name; ?></h3>
-<?php $day_id = 1; ?>
 <?php foreach ($irregular_shift_days as $irregular_shift_day): ?>
-	<?php if($irregular_shift_users{$day_id}): ?>
-		<h4><?php echo date( 'Y年m月d日', strtotime($irregular_shift_day->irregular_day_date)); ?></h4>
-		<table border="1" class="uk-table uk-width-7-10">
+	<h4><?php echo date( 'Y年m月d日', strtotime($irregular_shift_day->irregular_day_date)); ?></h4>
+	<table border="1" class="uk-table uk-width-7-10">
+		<thead>
 			<tr>
 				<td>スタッフ名</td>
 				<td>勤務形態</td>
@@ -12,21 +11,26 @@
 				<td>午前勤務</td>
 				<td>午後勤務</td>
 			</tr>
-			<?php foreach (${'irregular_shift_users'.$day_id} as $irregular_shift_user): ?>
-				<tr>
-					<td><?php echo $irregular_shift_user->users->full_name; ?></td>
-					<?php Helper_Shift::irregular_work_type_for_table($irregular_shift_user->edited_shift_type); ?>
-				</tr>
-			<?php endforeach; ?>
-			<tr>
-				<td><?php echo count(${'irregular_shift_users'.$day_id}); ?>名</td>
-				<td colspan="2">合計勤務時間</td>
-				<td><?php Helper_Shift::irregular_work_time_count($irregular_shift_day->id); ?>時間</td>
-				<td><?php Helper_Shift::irregular_work_staff_count($irregular_shift_day->id ,1); ?></td>
-				<td><?php Helper_Shift::irregular_work_staff_count($irregular_shift_day->id ,2); ?></td>
-			</tr>
-		</table>
-		<br />
-		<?php $day_id++; ?>
-	<?php endif; ?>
+		</thead>
+		<tbody id = "<?php echo $irregular_shift_day->id; ?>">
+			<script type="text/javascript">
+				$(function() {
+					var url = '../shift_detail/';
+					url += <?php echo $irregular_shift_day->id; ?>;
+					$.ajax(url, {"complete": function(xhr,status){
+						window.xhr = xhr;
+						$("#<?php echo $irregular_shift_day->id; ?>").html($(xhr.responseText));
+					}});
+				});
+			</script>
+		</tbody>
+		<tr>
+			<td>名</td>
+			<td colspan="2">合計勤務時間</td>
+			<td><?php Helper_Shift::irregular_work_time_count($irregular_shift_day->id); ?>時間</td>
+			<td><?php Helper_Shift::irregular_work_staff_count($irregular_shift_day->id ,1); ?></td>
+			<td><?php Helper_Shift::irregular_work_staff_count($irregular_shift_day->id ,2); ?></td>
+		</tr>
+	</table>
 <?php endforeach; ?>
+
---
+++ fuel/app/views/shift/irregular/shift_detail.php
@@ -0,0 +1,7 @@
+<?php foreach ($irregular_shift_users as $irregular_shift_user): ?>
+	<tr>
+		<td><?php echo $irregular_shift_user->users->full_name; ?></td>
+		<?php Helper_Shift::irregular_work_type_for_table($irregular_shift_user->edited_shift_type); ?>
+	</tr>
+<?php endforeach; ?>
+	
