29b4cf0e7c01557a646e42063978ff70af33a832
morishita <tomohiro.m0219@gmail.com>
2014-01-24 20:20:06 +0900

    #108 シフト編成のリンクを削除し、該当VCの削除

--- fuel/app/views/manage/shift/index.php
+++ fuel/app/views/manage/shift/index.php
@@ -14,7 +14,7 @@
 				<td><?php echo $irregular_shift->irregular_name; ?></td>
 				<td><?php Helper_Shift::shift_condition($irregular_shift->irregular_condition); ?></td>
 				<td><?php echo Html::anchor('shift/irregular/detail/'.$irregular_shift->id, '確認',array('class'=>'uk-button')); ?></td>
-				<td><?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$irregular_shift->id.'/info', '編集',array('class'=>'uk-button')); ?></td>
+				<td><?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$irregular_shift->id.'/info', '<i class="fa fa-inbox">シフト編集',array('class'=>'uk-button')); ?></td>
 				<td><?php echo Html::anchor('shift/irregular/output_pdf/'.$irregular_shift->id, 'PDF',array('class'=>'uk-button')); ?></td>
 			</tr>
 		<?php endforeach; ?>
