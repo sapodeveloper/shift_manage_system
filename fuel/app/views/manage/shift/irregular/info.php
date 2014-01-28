e0fde78a5697e5e8b47ed02358dd3ee531abc4c5
morishita <tomohiro.m0219@gmail.com>
2014-01-28 14:55:47 +0900

    #115 新レイアウトのみをまずは適用

--- fuel/app/views/manage/shift/irregular/info.php
+++ fuel/app/views/manage/shift/irregular/info.php
@@ -1,5 +1,4 @@
-[<?php echo $irregular_shift->irregular_name; ?>]の編集
-<br><br>
+<h6 class="uk-article-lead">[<?php echo $irregular_shift->irregular_name; ?>]の編集</h6>
 <ul class="uk-tab uk-tab-grid uk-tab-bottom" id="tab">
 	<li class="uk-width-1-<?php echo count(${'irregular_shift_days'})+1; ?>">
 		<?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$irregular_shift->id.'/info', '概要'); ?>
@@ -10,3 +9,82 @@
 		</li>
 	<?php endforeach; ?>
 </ul>
+
+<div class="uk-grid">
+	<div class="uk-width-medium-4-6">
+		<br>
+		<div> 前期レギュラーシフト</div>
+		<div class="uk-alert uk-alert-success uk-text-center"><i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;作成日&nbsp;&nbsp;2014年4月20日</div>
+		<div class="uk-alert uk-alert-danger uk-text-center"><i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;入力期限&nbsp;&nbsp;2014年4月28日</div>
+		<div>エントリー状況</div>
+		<div class="uk-progress uk-progress-striped uk-progress-mini uk-progress-success ">
+		<div class="uk-progress-bar  uk-progress-mini uk-progress-success " data-uk-tooltip title="40人中、10人がエントリー済みです。" style="width: 30%;"></div>
+		</div>
+		<div>個別勤務時間</div>
+		<table border="1" width=100% class="uk-text-center">
+			<tr>
+				<td colspan=1>名前</td>
+				<td colspan=2>1月1日(水)</td>
+				<td colspan=2>1月2日(木)</td>
+				<td colspan=2>1月3日(金)</td>
+				<td colspan=2>1月4日(土)</td>
+				<td colspan=2>1月5日(日)</td>
+			</tr>
+			<tr>
+				<td>豊嶋駿仁</td>
+				<td><i class="fa fa-sun-o"></i></td>
+				<td><i class="fa fa-moon-o"></i></td>
+				<td><i class="fa fa-sun-o"></i></td>
+				<td><i class="fa fa-moon-o"></i></td>
+				<td><i class="fa fa-sun-o"></i></td>
+				<td><i class="fa fa-moon-o"></i></td>
+				<td><i class="fa fa-sun-o"></i></td>
+				<td><i class="fa fa-moon-o"></i></td>
+				<td><i class="fa fa-sun-o"></i></td>
+				<td><i class="fa fa-moon-o"></i></td>
+			</tr>
+			<tr>
+				<td>三澤湧樹</td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-sun-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-moon-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-sun-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-moon-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-sun-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-moon-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-sun-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-moon-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-sun-o"></i></td>
+				<td style="background-color: #a9f5a9;"><i class="fa fa-moon-o"></i></td>
+			<tr>
+		</table>
+		<br>
+
+		<div class="uk-button-group">
+			<button class="uk-button uk-button-primary" type="submit">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-file-text"></i>&nbsp;PDF&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+			<button class="uk-button uk-button-success" type="submit">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-cloud-upload"></i>&nbsp;公開&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+			<button class="uk-button uk-button-danger" href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-minus-circle"></i>&nbsp;募集停止&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+		</div>
+	</div>
+	<div class="uk-width-medium-2-6">
+		<br>
+		<table border="1" width=100% class="uk-text-center">
+			<tbody>
+				<tr>
+					<td>スタッフ名</td>
+					<td>時間</td>
+					<td>日数</td>
+				</tr>
+				<tr>
+					<td>豊嶋駿仁</td>
+					<td>-</td>
+					<td>-</td>
+				</tr>
+				<tr>
+					<td>三澤湧樹</td>
+					<td>30:00</td>
+					<td>5</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
