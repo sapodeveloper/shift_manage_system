c458624fc6c2d7058f3a8cccbfa449bd5a8176a4
Edy Toyoshima <a213139@cc.it-hiroshima.ac.jp>
2014-02-04 13:34:08 +0900

    #130-Edy 各ページでのデザインの統一

--- fuel/app/views/manage/shift/index.php
+++ fuel/app/views/manage/shift/index.php
@@ -14,7 +14,7 @@
 				<td><?php echo $irregular_shift->irregular_name; ?></td>
 				<td><?php Helper_Shift::shift_condition($irregular_shift->irregular_condition); ?></td>
 				<td><?php echo Html::anchor('shift/irregular/detail/'.$irregular_shift->id, '<i class="fa fa-search">&nbsp;確認',array('class'=>'uk-button uk-button-success')); ?></td>
-				<td><?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$irregular_shift->id.'/info', '<i class="fa fa-inbox">&nbsp;シフト編集',array('class'=>'uk-button uk-button-primary')); ?></td>
+				<td><?php echo Html::anchor('manage/shift/irregular/edit_shift/'.$irregular_shift->id.'/info', '<i class="fa fa-pencil-square-o"></i>&nbsp;シフト編集',array('class'=>'uk-button uk-button-primary')); ?></td>
 				<td><?php echo Html::anchor('shift/irregular/output_pdf/'.$irregular_shift->id, '<i class="fa fa-picture-o">&nbsp;PDF',array('class'=>'uk-button uk-button-danger')); ?></td>
 			</tr>
 		<?php endforeach; ?>
--- fuel/app/views/manage/shift/irregular/create.php
+++ fuel/app/views/manage/shift/irregular/create.php
@@ -6,15 +6,15 @@
 	 
 	<table class="uk-table" >
 		<tr>
-			<td><i class="fa fa-pencil"></i>シフト名：</td>
+			<td><i class="fa fa-pencil"></i>&nbsp;シフト名：</td>
 			<td><?php echo Form::input('irregular_name', Input::post('irregular_name', isset($irregular) ? $irregular->irregular_name : ''), array('class' => '', 'placeholder'=>'新規シフト名入力')); ?></td>
 		</tr>
 		<tr>
-			<td><i class="fa fa-clock-o"></i>申請締切日：</td>
+			<td><i class="fa fa-clock-o"></i>&nbsp;申請締切日：</td>
 			<td><input type="text" data-uk-datepicker="{ weekstart:0, format:'YYYY/MM/DD', i18n: { months:['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'], weekdays:['日','月','火','水','木','金','土'] }}" size=10 placeholder="入力期限" readonly name="limitdate" value=""></td>
 		</tr>
 		<tr>
-			<td rowspan="5"><i class="fa fa-clock-o"></i>入力日</td>
+			<td rowspan="5"><i class="fa fa-clock-o"></i>&nbsp;入力日</td>
 			<td><input type="text" data-uk-datepicker="{ weekstart:0, format:'YYYY/MM/DD', i18n: { months:['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'], weekdays:['日','月','火','水','木','金','土'] }}" size=10 placeholder="1日目" readonly name="irregular_date[]" value=""></td>
 		</tr>
 		<tr>
@@ -30,11 +30,15 @@
 			<td><input type="text" data-uk-datepicker="{ weekstart:0, format:'YYYY/MM/DD', i18n: { months:['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'], weekdays:['日','月','火','水','木','金','土'] }}" size=10 placeholder="5日目" readonly name="irregular_date[]" value=""></td>
 		</tr>
 		<tr>
-			<td colspan="2"><?php echo Form::submit('submit', 'シフト作成', array('class' => 'uk-button')); ?></td>
+			<td colspan="2">
+				<button class="uk-button uk-button-primary" type="submit">
+					<i class="fa fa-pencil"></i>&nbsp;シフト作成
+				</button>
+			</td>
 		</tr>
 	</table>
 	<?php echo Form::close(); ?>
 	</div>
 </div>
-<?php echo Html::anchor('manage/shift/new', 'シフト作成選択',array('class'=>'uk-button')); ?>
+<?php echo Html::anchor('manage/shift/new', '<i class="fa fa-check-square-o"></i>&nbsp;シフト作成選択',array('class'=>'uk-button uk-button-success')); ?>
 
