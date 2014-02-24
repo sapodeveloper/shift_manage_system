fe5b9020076667972c9b3ca851d3d7b79e5081da
Y.Mizawa <b113229@cc.it-hiroshima.ac.jp>
2014-02-24 12:09:13 +0900

    issue98-mizawa いろいろと2

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,6 +2,18 @@
 return array(
 	//標準を認証画面に
 	'_root_'  => 'auth/login',
+
+	//manageコントローラ以下レギュラーシフト関係のルーティング
+	'manage/main/' => 'manage/main/index',
+	'manage/shift/regular/edit_shift/:id/info' => 'manage/shift/regular/info/:id',
+	'manage/shift/regular/edit_shift/:id/entry_list' => 'manage/shift/regular/entry_list/:id',
+	'manage/shift/regular/edit_shift/:id/lock_user_shifts/:user_id/:regular_id' => 'manage/shift/regular/lock_user_shifts/:user_id/:regular_id',
+	'manage/shift/regular/edit_shift/:id/unlock_user_shifts/:user_id/:regular_id' => 'manage/shift/regular/unlock_user_shifts/:user_id/:regular_id',
+	'manage/shift/regular/edit_shift/:id/change_entry_condition' => 'manage/shift/regular/change_entry_condition/:id',
+	'manage/shift/regular/edit_shift/:id/change_condition' => 'manage/shift/regular/change_condition/:id',
+	'manage/shift/regular/edit_shift/:id/edit_shift_day/:day_id' => 'manage/shift/regular/edit_shift_day/:day_id',
+	'manage/shift/regular/edit_shift/:id/edit_shift_user/:regular_user_id' => 'manage/shift/regular/edit_shift_user/:regular_user_id',
+	'manage/shift/regular/edit_shift/:id/edit_user_shift/:regular_user_id/:regular_type_id' => 'manage/shift/regular/edit_user_shift/:regular_user_id/:regular_type_id',
 	
 	//manageコントローラ以下イレギュラーシフト関係のルーティング
 	'manage/main/' => 'manage/main/index',
