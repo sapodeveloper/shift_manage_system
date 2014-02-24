596b010b92021ec1f676d61503cd44703526d8f1
nishimoto <b111090@cc.it-hiroshima.ac.jp>
2014-02-24 15:55:02 +0900

    #102 ついに忘却の彼方! とりあえずユーザ側の申請画面に表示できるようにした[C

--- fuel/app/classes/controller/shift.php
+++ fuel/app/classes/controller/shift.php
@@ -16,6 +16,7 @@ class Controller_Shift extends Controller_Application
 	{
 		$data['receiving_regulars'] = Model_Regular::get_receiving_regulars(Helper_Application::get_today());
 		$data['receiving_irregulars'] = Model_Irregular::get_receiving_irregulars(Helper_Application::get_today());
+		$data['re_receiving_irregulars'] = Model_Irregular::get_re_receiving_irregulars(Helper_Application::get_today());
 		$view = View::forge('layout/application');
 		$view->contents = View::forge('shift/request', $data);
 		return $view;
--- fuel/app/views/shift/request.php
+++ fuel/app/views/shift/request.php
@@ -50,3 +50,27 @@
 </table>
 <?php endif; ?>
 
+<?php if($re_receiving_irregulars): ?>
+<h3>再申請受付中イレギュラーシフト</h3>
+<table class="uk-table uk-table-hover">
+  <thead>
+    <tr>
+      <th>シフト名</th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($re_receiving_irregulars as $rr_irregular): ?>
+      <tr>
+        <td><?php echo $rr_irregular->irregular_name; ?></td>
+        <td><?php echo Html::anchor('irregular/request/'.$rr_irregular->id, '<i class="fa fa-upload"></i>&nbsp;申請', array('class' => 'uk-button uk-button-primary')); ?></td>
+        <td><?php echo Html::anchor('irregular/request_detail/'.$rr_irregular->id, '<i class="fa fa-search"></i>&nbsp;申請確認', array('class' => 'uk-button uk-button-primary')); ?></td>
+        <td><?php echo Html::anchor('shift/irregular/detail/'.$rr_irregular->id, '<i class="fa fa-search"></i>&nbsp;全体確認', array('class' => 'uk-button uk-button-success')); ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
+
