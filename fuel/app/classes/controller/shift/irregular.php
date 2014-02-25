404c649ce6c1f3f7bd2c5e6ba5d70c0d4ff7a3f4
morishita <tomohiro.m0219@gmail.com>
2014-02-25 21:38:44 +0900

    #167 PDFにデザインを可能な限り適用。リファクタリングしたい．．．

--- fuel/app/classes/controller/shift/irregular.php
+++ fuel/app/classes/controller/shift/irregular.php
@@ -32,72 +32,100 @@ class Controller_Shift_Irregular extends Controller
 		// 指定されたidのイレギュラーシフトを取得する	
 		$irregular_shift = Model_Irregular::find($id);
 		$irregular_shift_days = Model_Irregular_Day::find('all', array('where' => array('irregular_id' => $id)));
-		$irregular_shift_users = Model_Irregular_User::find('all', array('where' => array('irregular_day_id' => 1)));
 		foreach ($irregular_shift_days as $irregular_shift_day) {
 			if($irregular_shift_day->id > 5){
 				$day_id = ($irregular_shift_day->id)%5+1;
 			}else{
 				$day_id = $irregular_shift_day->id;
 			}
-			$irregular_shift_users{$day_id} = Model_Irregular_User::find('all', array('where' => array('irregular_day_id' => $irregular_shift_day->id)));
+			$irregular_shift_users{$day_id} = Model_Irregular_User::find('all', array('where' => array(array('irregular_day_id' => $irregular_shift_day->id), array('edited_shift_type', 'in', array(1,2,3)))));
 		}
+		$query = DB::query('SELECT distinct irregular_user.user_id, users.full_name 
+			from irregular_user 
+			inner join users on users.id = irregular_user.user_id
+			where irregular_day_id in (SELECT id FROM irregular_day WHERE irregular_id = '.$id.')
+			and edited_shift_type in (1,2,3)
+			order by irregular_user.user_id');
+		$irregular_users = $query->as_object()->execute()->as_array();
 		$css = '<style> td.bg { background-color: #00bfff; } </style>';
-		$html = '<meta charset="utf-8"><h3>';
+		$html = '<meta charset="utf-8"><br><div><center><font size="15">';
 		$html .= $irregular_shift->irregular_name;
-		$html .= '</h3>';
-		$day_id = 1;
-		foreach ($irregular_shift_days as $irregular_shift_day){
-			if($irregular_shift_users{$day_id}){
-				$html .= '<h4>';
-				$html .= date( 'Y年m月d日', strtotime($irregular_shift_day->irregular_day_date));
-				$html .= '</h4><table border="1"><tr><td>スタッフ名</td><td>勤務形態</td><td>勤務時間</td><td>時間</td><td>午前勤務</td><td>午後勤務</td></tr>';
-				// 該当日の合計勤務時間
-				$total_work_time = 0;
-				// 該当日の午前勤務者数
-				$morining_work_staff_num = 0;
-				// 該当日の午後勤務者数
-				$afternoon_work_staff_num = 0;
-				foreach ($irregular_shift_users{$day_id} as $irregular_shift_user){
-					$html .= '<tr><td>';
-					$html .= $irregular_shift_user->users->full_name;
-					$html .= '</td>';
-					if($irregular_shift_user->edited_shift_type == 1){
-						$html .= "<td>午前勤務</td><td>10:00 ~ 13:00</td><td>3:00</td><td class=\"bg\"></td><td></td>";
-						$total_work_time += 3;
-						$morining_work_staff_num++;
-					}elseif($irregular_shift_user->edited_shift_type == 2){
-						$html .= "<td>午後勤務</td><td>13:00 ~ 17:00</td><td>4:00</td><td></td><td class=\"bg\"></td>";
-						$total_work_time += 4;
-						$afternoon_work_staff_num++;
-					}elseif($irregular_shift_user->edited_shift_type == 3){
-						$html .= "<td>フル勤務</td><td>10:00 ~ 17:00</td><td>6:00</td><td class=\"bg\"></td><td class=\"bg\"></td>";
-						$total_work_time += 6;
-						$morining_work_staff_num++;
-						$afternoon_work_staff_num++;
+		$html .= '</font></center></div><br>';
+		$html .= '<table><tr><td width="580">';
+			$day_id = 1;
+			foreach ($irregular_shift_days as $irregular_shift_day){
+				if($irregular_shift_users{$day_id}){
+					$html .= '<i style="center">';
+					$html .= date( 'Y年m月d日', strtotime($irregular_shift_day->irregular_day_date));
+					$html .= '</i><br><font size="11"><table border="1" ><tr><td width="15%">スタッフ名</td><td width="15%">勤務時間</td><td width="15%">時間</td><td width="25%">午前勤務</td><td width="25%">午後勤務</td></tr>';
+					// 該当日の合計勤務時間
+					$total_work_time = 0;
+					// 該当日の午前勤務者数
+					$morining_work_staff_num = 0;
+					// 該当日の午後勤務者数
+					$afternoon_work_staff_num = 0;
+					foreach ($irregular_shift_users{$day_id} as $irregular_shift_user){
+						$html .= '<tr><td>';
+						$html .= $irregular_shift_user->users->full_name;
+						$html .= '</td>';
+						if($irregular_shift_user->edited_shift_type == 1){
+							$html .= "<td>10:00 - 13:00</td><td>3:00</td><td class=\"bg\"></td><td></td>";
+							$total_work_time += 3;
+							$morining_work_staff_num++;
+						}elseif($irregular_shift_user->edited_shift_type == 2){
+							$html .= "<td>13:00 - 17:00</td><td>4:00</td><td></td><td class=\"bg\"></td>";
+							$total_work_time += 4;
+							$afternoon_work_staff_num++;
+						}elseif($irregular_shift_user->edited_shift_type == 3){
+							$html .= "<td>10:00 - 17:00</td><td>6:00</td><td class=\"bg\"></td><td class=\"bg\"></td>";
+							$total_work_time += 6;
+							$morining_work_staff_num++;
+							$afternoon_work_staff_num++;
+						}
+						$html .= '</tr>';
 					}
-					$html .= '</tr>';
+					$html .= '<tr><td>';
+					$html .= count($irregular_shift_users{$day_id});
+					$html .= '名</td><td>合計勤務時間</td><td>';
+					$html .= $total_work_time;
+					$html .= '時間</td><td>';
+					$html .= $morining_work_staff_num;
+					$html .= '</td><td>';
+					$html .= $afternoon_work_staff_num;
+					$html .= '</td></tr></table></font><br>';
 				}
-				$html .= '<tr><td>';
-				$html .= count($irregular_shift_users{$day_id});
-				$html .= '名</td><td>合計勤務時間</td><td></td><td>';
-				$html .= $total_work_time;
-				$html .= '時間</td><td>';
-				$html .= $morining_work_staff_num;
-				$html .= '</td><td>';
-				$html .= $afternoon_work_staff_num;
-				$html .= '</td></tr></table>';
+				$day_id++;
 			}
-			$day_id++;
+		$html .= '</td><td width="200">';
+		$html .= '<i>トータル勤務日数・時間</i><br>';
+		$html .= '<table border="1" ><tr><td>スタッフ名</td><td>勤務日数</td><td>勤務時間</td></tr>';
+		foreach ($irregular_users as $irregular_user){
+			$dwdc = 0;
+			$html .= '<tr><td>';
+			$html .= $irregular_user->full_name;
+			$html .= '</td><td>';
+			$html .= Helper_Shift_Irregular::deside_work_day_count_pdf($irregular_shift->id, $irregular_user->user_id);
+			$html .= '日';
+			$html .= '</td><td>';
+			$html .= Helper_Shift_Irregular::deside_work_time_count_pdf($irregular_shift->id, $irregular_user->user_id);
+			$html .= ':00';
+			$html .= '</td></tr>';
 		}
+		$html .= '<tr><td>合計</td><td>-----</td><td>';
+		$html .= Helper_Shift_Irregular::deside_irregular_group_total_work_time_pdf($irregular_shift->id);
+		$html .= ':00</td></tr></table>';
+		$html .= '</td></tr></table>';
+		
 		//return $css . $html;
 		Package::load('pdf');
-    $pdf = Pdf::factory('tcpdf')->init("P", "mm", "A3", true, "UTF-8");
-    $pdf->setPrintHeader(false);
-    $pdf->setPrintFooter(false);
-    $pdf->AddPage();
-    $pdf->SetFont('kozminproregular', '', 12);
-    $pdf->writeHTML($css.$html, false, false, false, false, 'C');
-    $pdf->Output("Irregular_Shift_No".$irregular_shift->id.".pdf", "I");
+	    $pdf = Pdf::factory('tcpdf')->init("P", "mm", "A3", true, "UTF-8");
+	    $pdf->setPrintHeader(false);
+	    $pdf->setPrintFooter(false);
+	    $pdf->AddPage();
+	    $pdf->SetFont('kozminproregular', '', 12);
+	    $pdf->writeHTML($css.$html, false, false, false, false, 'C');
+	    ob_end_clean();
+	    $pdf->Output("Irregular_Shift_No".$irregular_shift->id.".pdf", "I");
 	}
 
 }
--- fuel/app/classes/helper/shift/irregular.php
+++ fuel/app/classes/helper/shift/irregular.php
@@ -91,6 +91,17 @@ class Helper_Shift_Irregular{
     echo count($work_day);
   }
 
+  // 当該イレギュラーシフトグループの特定ユーザの確定勤務日数を求める(PDF用)
+  public static function deside_work_day_count_pdf($irregular_id, $user_id){
+    $irregular_day_id = DB::select('id')->from('irregular_day')->where('irregular_id', $irregular_id);
+    $work_day = DB::select('*')->from('irregular_user')
+        ->where('user_id', $user_id)
+        ->and_where('edited_shift_type', 'in', array(1,2,3))
+        ->and_where('irregular_day_id', 'in', $irregular_day_id)
+        ->execute();
+    return count($work_day);
+  }
+
   // 当該イレギュラーシフトグループの特定ユーザの希望勤務時間を求める
   public static function request_work_time_count($irregular_id, $user_id){
     $irregular_day_id = DB::select('id')->from('irregular_day')->where('irregular_id', $irregular_id);
@@ -133,6 +144,27 @@ class Helper_Shift_Irregular{
     echo count($morning)*3 + count($afternoon) * 4 + count($full) * 6;
   }
 
+  // 当該イレギュラーシフトグループの特定ユーザの確定勤務時間を求める(pdf用)
+  public static function deside_work_time_count_pdf($irregular_id, $user_id){
+    $irregular_day_id = DB::select('id')->from('irregular_day')->where('irregular_id', $irregular_id);
+    $morning = DB::select('*')->from('irregular_user')
+        ->where('user_id', $user_id)
+        ->and_where('edited_shift_type', 1)
+        ->and_where('irregular_day_id', 'in', $irregular_day_id)
+        ->execute();
+    $afternoon = DB::select('*')->from('irregular_user')
+        ->where('user_id', $user_id)
+        ->and_where('edited_shift_type', 2)
+        ->and_where('irregular_day_id', 'in', $irregular_day_id)
+        ->execute();
+    $full = DB::select('*')->from('irregular_user')
+        ->where('user_id', $user_id)
+        ->and_where('edited_shift_type', 3)
+        ->and_where('irregular_day_id', 'in', $irregular_day_id)
+        ->execute();
+    return count($morning)*3 + count($afternoon) * 4 + count($full) * 6;
+  }
+
   // 当該イレギュラーシフトグループの希望勤務時間の合計を求める
   public static function request_irregular_group_total_work_time($irregular_id){
     $irregular_day_id = DB::select('id')->from('irregular_day')->where('irregular_id', $irregular_id);
@@ -173,6 +205,27 @@ class Helper_Shift_Irregular{
         ->and_where('edited_shift_type', 3)
         ->execute();
     echo count($morning)*3 + count($afternoon) * 4 + count($full) * 6;
+  }
+
+  // 当該イレギュラーシフトグループの確定勤務時間の合計を求める(PDF用)
+  public static function deside_irregular_group_total_work_time_pdf($irregular_id){
+    $irregular_day_id = DB::select('id')->from('irregular_day')->where('irregular_id', $irregular_id);
+    $morning = DB::select('*')
+        ->from('irregular_user')
+        ->where('irregular_day_id', 'in', $irregular_day_id)
+        ->and_where('edited_shift_type', 1)
+        ->execute();
+    $afternoon = DB::select('*')
+        ->from('irregular_user')
+        ->where('irregular_day_id', 'in', $irregular_day_id)
+        ->and_where('edited_shift_type', 2)
+        ->execute();
+    $full = DB::select('*')
+        ->from('irregular_user')
+        ->where('irregular_day_id', 'in', $irregular_day_id)
+        ->and_where('edited_shift_type', 3)
+        ->execute();
+    return count($morning)*3 + count($afternoon) * 4 + count($full) * 6;
   }
 
   // 当該レギュラーシフトグループにおいて特定ユーザが編集ロックされているか調べる
