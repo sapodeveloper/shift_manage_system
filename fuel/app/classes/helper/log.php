9618d2e63999c2e35c36e66b6435f4afa1cc798f
morishita <tomohiro.m0219@gmail.com>
2015-01-11 19:12:08 +0900

    #184 Helper::Logのコメント記載

--- fuel/app/classes/helper/log.php
+++ fuel/app/classes/helper/log.php
@@ -1,5 +1,10 @@
 <?php
 class Helper_Log {
+	/*
+		引数：ログタイプ、ログ、ログ管理
+		戻り値：なし
+		用途：処理実行時のログを残すための処理を行う
+	*/
     public static function write_log($log_type, $log_message, $log_condition) {
         $log = Model_Log::forge(array(
             'log_type' => $log_type,
